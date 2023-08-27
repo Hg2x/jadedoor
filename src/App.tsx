9fb525e82de4e7474e2f0baa3bc01d46d6b246f9
h2 <wnhg2czh@gmail.com>
2023-08-27 21:02:32 +0800

    Basic prototype generate reply frontend
    Replace starter boilerplate code with initial prototype code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// npm start
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App: React.FC = () => {
+  const [userPrompt, setUserPrompt] = useState('');
+  const [generatedResult, setGeneratedResult] = useState<string | null>(null);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserPrompt(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('http://localhost:8000/', {
+        user_prompt: userPrompt,
+      });
+      setGeneratedResult(response.data.generated_result);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={userPrompt}
+        onChange={handleInputChange}
+        placeholder="Enter your prompt"
+      />
+      <button onClick={handleSubmit}>Generate Reply</button>
+      {generatedResult && <div>Response: {generatedResult}</div>}
     </div>
   );
-}
+};
 
 export default App;
